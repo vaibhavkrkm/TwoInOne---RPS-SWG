07e207a3b3e0ade52420dcd8b0d5bdb277ca48fc
Vaibhav Kumar <vaibhavkrkm@gmail.com>
2020-10-05 10:44:41 +0530

    Added the Back to Menu Functionality and done a few Minor Fixes

---
+++ Game/rps_mainlogic.js
@@ -0,0 +1,26 @@
+function backToMenu()
+{
+	window.history.back();
+}
+
+
+var rockButton = document.getElementById("rock");
+var scissorButton = document.getElementById("scissor");
+var paperButton = document.getElementById("paper");
+var backButton = document.getElementById("goBack");
+
+rockButton.addEventListener("click", function(){                               // rock button
+	
+})
+
+scissorButton.addEventListener("click", function(){                         // scissor button
+	
+})
+
+paperButton.addEventListener("click", function(){                           // paper button
+	
+})
+
+backButton.addEventListener("click", function(){                              // back button for going back to main menu
+	backToMenu();
+})
---
+++ Game/swg_mainlogic.js
@@ -0,0 +1,26 @@
+function backToMenu()
+{
+	window.history.back();
+}
+
+
+var snakeButton = document.getElementById("snake");
+var waterButton = document.getElementById("water");
+var gunButton = document.getElementById("gun");
+var backButton = document.getElementById("goBack");
+
+snakeButton.addEventListener("click", function(){                               // snake button
+	
+})
+
+waterButton.addEventListener("click", function(){                         // water button
+	
+})
+
+gunButton.addEventListener("click", function(){                           // gun button
+	
+})
+
+backButton.addEventListener("click", function(){                              // back button for going back to main menu
+	backToMenu();
+})
